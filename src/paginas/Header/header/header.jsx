e4aa8b0ab1fdfabfbd6492a621b201c0de1806be
pedro <nickapablaza.com@gmail.com>
2023-11-16 14:14:20 -0300

    .

--- src/paginas/Header/header/header.jsx
+++ src/paginas/Header/header/header.jsx
@@ -43,12 +43,12 @@ function Header() {
 
           <div className="menu-desktop">
             <nav>
-              <Link to={'/'}>Home</Link>&nbsp;&nbsp;&nbsp;
-              <Link to={'/Sobre'}>Sobre</Link>&nbsp;&nbsp;&nbsp;
-              <Link to={'/Publico'}>Público Alvo</Link>&nbsp;&nbsp;&nbsp;
-              <Link to={'/Diferencial'}>Diferencial</Link>&nbsp;&nbsp;&nbsp;
-              <Link to={'/Equipamentos'}>Equipamentos</Link>&nbsp;&nbsp;&nbsp;
-              <Link to={'/Bibliografia'}>Bibliografia</Link>&nbsp;&nbsp;&nbsp;
+              <Link to={'/'}>Home</Link>
+              <Link to={'/Sobre'}>Sobre</Link>
+              <Link to={'/Publico'}>Público Alvo</Link>
+              <Link to={'/Diferencial'}>Diferencial</Link>
+              <Link to={'/Equipamentos'}>Equipamentos</Link>
+              <Link to={'/Bibliografia'}>Bibliografia</Link>
             </nav>
           </div>
         </div>
